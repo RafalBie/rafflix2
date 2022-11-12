173515334cd8e7c8364203bc8c125fc675dafaf6
RafalBie <50397279+RafalBie@users.noreply.github.com>
2022-11-12 20:13:42 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+// import axios from './axios';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hey Programers</h1>
+    <Row title="REMFLIX ORIGINALS"  />
+			<Row title="Trending"  />
+		
+    </div>
+  );
+}
+
+export default App;
